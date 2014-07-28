1fda2f08ba3f78acd36f8d66050a7c5eeadd1747
l3mce <jason.lester@linuxmce.org>
2014-07-27 23:16:14 -0400

    updates

---
+++ installs.rb
@@ -0,0 +1,17 @@
+package 'VirtualBox-4.3'
+package 'binutils'
+package 'qt'
+package 'gcc'
+package 'make'
+package 'patch'
+package 'libgomp'
+package 'glibc-devel'
+package 'kernel-headers'
+package 'kernel-devel'
+package 'dkms'
+package 'git'
+package 'fpaste'
+ 
+ 
+
+
